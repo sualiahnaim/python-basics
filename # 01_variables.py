ebe70ee4877b215c5818a0dfc2511a35f1106457
sualiahnaim <sualiahnaim@gmail.com>
2025-08-05 22:42:20 +0530

    Added variables file

---
+++ # 01_variables.py
@@ -0,0 +1,13 @@
+# 01_variables.py
+
+# Variable Types
+name = "Sualiah"
+age = 21
+height = 5.8
+is_student = True
+
+# Print Statement
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Student:", is_student)
